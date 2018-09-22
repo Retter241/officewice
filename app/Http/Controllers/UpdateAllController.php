ad277242933363853dd8cc7ffa8b769f2d868b8c
Retter241 <retter241@gmail.com>
2018-09-22 11:58:37 +0300

    add bitrix clean response rest -  deals ( +one deal )

--- app/Http/Controllers/UpdateAllController.php
+++ app/Http/Controllers/UpdateAllController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Deal;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use DB; //фасад
+
+use Carbon\Carbon; //обработка даты
 
 class UpdateAllController extends Controller
 {
@@ -23,12 +27,12 @@ class UpdateAllController extends Controller
 
         $bx_response[] = $this->callB24Method($this->auth, $this->method, $this->params);
 
-        $build_data = $this->buildData($bx_response);
-
-        $clear_data = $this->processingData($build_data);
+        $clear_data = $this->buildData($bx_response);
 
 
         $response_message = $this->addDataToDb($clear_data);
+
+
 
 
         return view('update.index')->with('response_message', $response_message);
@@ -43,7 +47,7 @@ class UpdateAllController extends Controller
     {
 
 
-        $c = curl_init('https://' . $auth['domain'] . '/rest/' . $method . '.json?select[0]=UF_*');
+        $c = curl_init('https://' . $auth['domain'] . '/rest/' . $method . '.json?select[0]=UF_*&select[1]=*');
         //$params["auth"] = $auth["access_token"];
         curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($c, CURLOPT_POST, true);
@@ -59,18 +63,64 @@ class UpdateAllController extends Controller
 
     /*
      * принимает черновой массив
-     *
-     * возвращает чистый массив нужных сделок
+     * формирует массив необходимой нам замены(googleMaps etc)
+     * возвращает массив нужных сделок для записи в БД
      * */
-    public function buildData(array $result)
+    public function buildData(array $bx_response)
     {
 
 
         $data = [];
-        foreach ($result as $item => $list) {
+        $pattern = "#^[0]:\s#";
+
+        /*
+         * STAGE_ID  NEW -> 1 - в обработке -> 2- проверена страховка перевозчика 3-> получена заявка
+         *
+         */
+
+        foreach ($bx_response as $item => $list) {
             foreach ($list as $k => $v) {
-                if ($v['UF_SH_LOCATION_FROM'] !== NULL) {
-                    $data[$k] = $v;
+                $re = preg_match($pattern, $v['TITLE']);
+                //$v['UF_SH_LOCATION_FROM']) !== NULL and ;
+                if (/*$re and*/
+                    $v['OPENED'] == 'Y' and $v['CLOSED'] == 'N') {
+
+                    //$data[$k] = $v; // All Data for test
+
+                    $locations = $this->parseLocation($v['UF_SH_LOCATION_FROM']);
+
+                    //стадии сделок - после 3й исполнитель выбран - ?
+                    $status = '';
+                    if ($v['STAGE_ID'] === 'NEW') {
+                        $status .= 'Новая - Да';
+                    } elseif ($v['STAGE_ID'] === 1) {
+                        $status .= 'В оработке';
+                    } elseif ($v['STAGE_ID'] === 2) {
+                        $status .= 'Проверена страховка перевозчика';
+                    } elseif ($v['STAGE_ID'] < 3) {
+                        $status .= 'Получена заявка';
+                    } else {
+                        $status .= 'Счет на предопл. заказчику...{ и дальше нам не надо?}';
+                    }
+
+
+                    $data[$k]['bitrix_id'] = $v['ID'];
+                    $data[$k]['deal_status'] = $status;  //["OPENED"]=> string(1) "Y"
+                    //$data[$k]['deal_number'] = $v['TITLE'];
+                    $data[$k]['deal_title'] = $v['UF_CRM_D_ITINERARY'];
+
+                    $data[$k]['deal_location_from'] = $locations['from'][2];
+                    $data[$k]['deal_location_from_city'] = $locations['from'][0] . ' (' . $locations['from'][1] . ')';
+                    $data[$k]['deal_location_to'] = $locations['to'][2];
+                    $data[$k]['deal_location_to_city'] = $locations['to'][0] . ' (' . $locations['to'][1] . ')';
+                    $data[$k]['deal_location_across'] = $locations['across']; //&
+
+                    $data[$k]['deal_delivery_date'] = $this->dateParse($v['UF_CRM_1483628352']);//Срок доставки груза:
+                    $data[$k]['deal_loading_date'] = $this->dateParse($v['UF_CRM_1483628167']); //Дата и время подачи транспортного средства:
+                    $data[$k]['deal_transport_type'] = '{Требования к автотранспорту:} ' . $v['UF_CRM_1483673991'];
+                    $data[$k]['deal_cargo_params'] = $v['UF_CRM_1474277632'] . ' {Обьем (м3) :}' . $v['UF_CRM_1467791573'] . '{ масса брутто ( кг ) :}' . $v['UF_CRM_1467791573'];
+                    $data[$k]['deal_special_conditions'] = ' { Способ погрузки:} ' . $v['UF_CRM_1483674041'];
+
                 }
             }
         }
@@ -79,29 +129,59 @@ class UpdateAllController extends Controller
         //return list of deals
     }
 
-    /*
-     * принимает массив нужных сделок
-     * обработка данных массива и
-     * формирование необходимой замены(googleMaps etc)
-     * возвращает массив для записи в БД
-     */
+    /*Принимает строку локации
+     *(string)L1145.286|AДзержинск=AНижегородская область=AРоссия=56.2440992=43.4351805|BМинск=BМинская область=BБеларусь=53.9045398=27.5615244
+     * парсит на состовляющие
+     * возвращает массив вида
+     * Array ( [from] => Array ( [0] => Дзержинск [1] => Нижегородская область [2] => Россия )
+     *         [to] => Array ( [0] => Минск [1] => Минская область [2] => Беларусь ) [across] => )
+     * */
 
-    public function processingData($build_data)
+    public function parseLocation($source)
     {
-        $clear_data =[];
-        foreach ($build_data as $k => $v) {
+        $res = explode('|', $source);
+//$res[0] - ненадо
 
-            if ($v['UF_CRM_D_ITINERARY'] !== NULL) {
+//$res[1] - откуда
+        $from = explode("A=", $res[1]);
+        $from = explode("=", $from[0]);
 
-                //$string = titleParse($v['UF_CRM_D_ITINERARY']);
-                //$clear_data['from'] = $string['from'];
+//$res[2] - куда
+        $to = explode("В=", $res[2]);
+        $to = explode("=", $to[0]);
+
+
+        foreach ($from as $key => $value) {
+            if (!is_numeric($value)) {
+                //$B = preg_match('/[A-ZА-Я]{2}/',  $str);
+                $location['from'][] = mb_substr($value, 1);
             }
-
         }
+        foreach ($to as $key => $value) {
+            if (!is_numeric($value)) {
+                //$B = preg_match('/[A-ZА-Я]{2}/',  $str);
+                $location['to'][] = mb_substr($value, 1);
+            }
+        }
+        $location['across'] = '';
 
-        $clear_data = $build_data;
-        return $clear_data;
+        return (array)$location;
     }
+
+    protected function dateParse($dateString)
+    {
+        //$date = array();
+        $date = explode('T', $dateString);
+        //$date = 'Дата: ' . date( 'd.m.y', strtotime($date[0])) .' | Время: ' . date('H:i', strtotime($date[1]));
+        $date['date'] = date('d.m.y', strtotime($date[0]));
+        $date['time'] = date('H:i', strtotime($date[1]));
+        $result['date'] = $date['date'];
+        $result['time'] = $date['time'];
+
+        return (array)$result;
+    }
+
+
     /*
      * принимает чистый массив
      * пишет в бд
@@ -109,25 +189,34 @@ class UpdateAllController extends Controller
      */
     public function addDataToDb($clear_data)
     {
+$rows = array();
+       foreach ($clear_data as $k=> $v){
+           $rows[$k]=$v;
 
-        /*/
-                need fields:
-                        ['ID']
-                        ['UF_SH_LOCATION_FROM']
+           $from = serialize($v['deal_location_from']);
+            $to = serialize($v['deal_location_to']);
+           $deal = new Deal;
 
-        */
+           $deal-> id_bitrix=$v['bitrix_id'] ;
+           $deal-> deal_status= $v['deal_status'];
+           $deal-> deal_number= $v['bitrix_id'];
+           $deal-> deal_title= $v['deal_title'];
+           $deal-> deal_location_across=$v['deal_location_across'] ;
+           $deal-> deal_delivery_date= $v['deal_delivery_date']['date'];
+           $deal-> deal_loading_date= $v['deal_loading_date']['date'];
+           $deal-> deal_location_from= $v['deal_location_from'];
+           $deal-> deal_location_to=  $v['deal_location_to'];
+           $deal-> deal_transport_type= $v['deal_transport_type'];
+           $deal-> deal_cargo_params= $v['deal_cargo_params'];
+           $deal-> deal_special_conditions= $v['deal_special_conditions'];
 
+           $deal->save();
+       }
 
-        $response_message = $clear_data;
+        $response_message = $rows;
 
         return $response_message;
     }
 
-    public function titleParse () {
 
-        //$string['from']
-        //$string['to']
-        $string = '';
-        return $string;
-    }
 }
--- resources/views/update/index.blade.php
+++ resources/views/update/index.blade.php
@@ -6,8 +6,46 @@
  * Time: 14:05
  */
 
-    ?>
+?>
 
 <pre>
-    {{ var_dump($response_message) }}
+ {{ var_dump($response_message) }}
 </pre>
+<!--
+<h3 style="width: 100%;">Все активные : opened=Y</h3>
+<table>
+
+    <tr>
+        <td>Битрикс id</td>
+        <td>Статус сделки</td>
+        <td>Название</td>
+        <td>Откуда</td>
+        <td>Куда</td>
+        <td>Дата подачи транспорта</td>
+        <td>Срок доставки груза</td>
+
+        <td>Требования к авто</td>
+        <td>Параметры груза</td>
+        <td>Способ погрузки</td>
+    </tr>
+
+
+    @foreach($response_message as $v)
+        <tr>
+            <td>{{ $v['bitrix_id'] }}</td>
+            <td>{{ $v['deal_status'] }}</td>
+            <td>{{ $v['deal_title'] }}</td>
+            <td>{{ $v['deal_location_from'] }}</td>
+            <td>{{ $v['deal_location_to'] }}</td>
+            <td>{{ $v['deal_loading_date']['date'] }}</td>
+            <td>{{ $v['deal_delivery_date']['date'] }}</td>
+
+            <td>{{ $v['deal_transport_type'] }}</td>
+            <td>{{ $v['deal_cargo_params'] }}</td>
+            <td>{{ $v['deal_special_conditions'] }}</td>
+        </tr>
+
+    @endforeach
+
+
+</table>-->
